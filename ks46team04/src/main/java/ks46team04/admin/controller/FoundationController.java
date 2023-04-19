9123bc11bd682c2eb178915f9a8b7b2b5ca1db01
gwirim <rnfka23@gmail.com>
2023-04-19 14:07:15 +0900

    [ksmart46team04] foundation html, Controller, Mapper, Service 수정 author: 김귀림 file: add_foundation.html, foundation_list.html, FoundationController.java, FoundationMapper.java, FoundationMapper.xml, FoundatonService.java detail: add_foundation.html, foundation_list.html, FoundationController.java, FoundationMapper.java, FoundatonService.java - 코드 수정,  FoundationMapper.xml - 파일 추가

--- ks46team04/src/main/java/ks46team04/admin/controller/FoundationController.java
+++ ks46team04/src/main/java/ks46team04/admin/controller/FoundationController.java
@@ -1,14 +1,29 @@
 package ks46team04.admin.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ks46team04.admin.dto.Foundation;
+import ks46team04.admin.service.FoundationService;
+
 @Controller
 @RequestMapping("/foundation")
 public class FoundationController {
 
+	private static final Logger log = LoggerFactory.getLogger(FoundationController.class);
+
+	private final FoundationService founationService;
+	
+	public FoundationController(FoundationService foundationService) {
+		this.founationService = foundationService;
+	}
+	
 	/**
 	 * 재단 정보 조회
 	 * @param model
@@ -17,8 +32,10 @@ public class FoundationController {
 	@GetMapping("/foundation_list")
 	public String getFoundationList(Model model) {
 		
-		model.addAttribute("title", "foundation_list");
-		model.addAttribute("content", "thymeleaf layout 완성");
+		List<Foundation> foundationList = founationService.getFoundationList();
+
+		model.addAttribute("title", "재단 목록");
+		model.addAttribute("foundationList", foundationList);
 		
 		return "view/foundation/foundation_list";
 	}
@@ -31,8 +48,7 @@ public class FoundationController {
 	@GetMapping("/add_foundation")
 	public String addFoundation(Model model) {
 		
-		model.addAttribute("title", "add_foundation");
-		model.addAttribute("content", "thymeleaf layout 완성");
+		model.addAttribute("title", "재단 등록");
 		
 		return "view/foundation/add_foundation";
 	}
@@ -45,7 +61,7 @@ public class FoundationController {
 	@GetMapping("/modify_foundation")
 	public String modifyFoundation(Model model) {
 		
-		model.addAttribute("title", "modify_foundation");
+		model.addAttribute("title", "재단 수정");
 		model.addAttribute("content", "thymeleaf layout 완성");
 		
 		return "view/foundation/modify_foundation";
--- ks46team04/src/main/java/ks46team04/admin/mapper/FoundationMapper.java
+++ ks46team04/src/main/java/ks46team04/admin/mapper/FoundationMapper.java
@@ -1,5 +1,23 @@
 package ks46team04.admin.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks46team04.admin.dto.Foundation;
+
+@Mapper
 public interface FoundationMapper {
 
+	// 재단 조회
+	public List<Foundation> getFoundationList();
+	
+	// 재단 등록
+	public int addFoundation(Foundation foundation);
+	
+	// 특정 재단 조회
+	public Foundation getFoundationByCode(String foundationCode);
+	
+	// 재단 수정
+	public int modifyFoundation(Foundation foundation);
 }
