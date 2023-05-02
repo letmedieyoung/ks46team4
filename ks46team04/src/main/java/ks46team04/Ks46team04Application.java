3f98753c9a9bd3a915603bd24aec7713eee4230c
LBR <fatrugi@gmail.com>
2023-05-02 17:41:38 +0900

    [ks46team4] master 배포용

--- ks46team04/src/main/java/ks46team04/Ks46team04Application.java
+++ ks46team04/src/main/java/ks46team04/Ks46team04Application.java
@@ -2,8 +2,10 @@ package ks46team04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Ks46team04Application {
 
 	public static void main(String[] args) {
