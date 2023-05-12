3e61ef703f83b33413da3dcc090af14a22c9d089
LBR <fatrugi@gmail.com>
2023-05-12 17:10:00 +0900

    [ks46team4] 재무 개발 시작 author: LBR detail:재무 진행 상황 주석 정리 file: ks46team04/src/main/resources/mapper/ProfitLossMapper.xml ks46team04/src/main/java/ks46team04/admin/service/ProfitLossService.java ks46team04/src/main/java/ks46team04/admin/dto/Purchase.java ks46team04/src/main/resources/templates/admin/purchase_sale/purchase_insert.html ks46team04/src/main/resources/templates/admin/purchase_sale/purchase_list.html ks46team04/src/main/resources/templates/admin/purchase_sale/purchase_update.html ks46team04/src/main/java/ks46team04/admin/controller/PurchaseController.java ks46team04/src/main/java/ks46team04/admin/mapper/PurchaseMapper.java ks46team04/src/main/resources/mapper/PurchaseMapper.xml ks46team04/src/main/java/ks46team04/admin/service/PurchaseService.java ks46team04/src/main/java/ks46team04/scheduling/SettlementScheduling.java

--- ks46team04/src/main/java/ks46team04/scheduling/SettlementScheduling.java
+++ ks46team04/src/main/java/ks46team04/scheduling/SettlementScheduling.java
@@ -19,7 +19,10 @@ public class SettlementScheduling {
 	private static final Logger log = LoggerFactory.getLogger(SettlementScheduling.class);
 
 	
-	//@Scheduled(cron = "0 0 0 1 * ?")
+	//@Scheduled(cron = "0 0 0 4 * ?")		//정해진 마감일
+	//@Scheduled(cron = "10 0 0 0 * ?")		//마감 테스트 스케줄 - 매 분의 10초에 실행(1분 간격)
+	//@Scheduled(cron = "*/10 0 0 0 * ?")	//마감 테스트 스케줄 - 10초마다 실행(10초 간격)
+	//@Scheduled(cron = "0 0 0 1 * ?")		//마감 테스트 스케줄
 	public void settlementProcess() {
 		log.info("3초마다 실행");
 	}
